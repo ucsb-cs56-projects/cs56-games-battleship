28745e0a5c8df39bacd0108ccf714816538e7591
Wei Guo <weiguo@umail.ucsb.edu>
2013-05-19 13:13:22 -0700

    modify the Ship model

--- src/edu/ucsb/cs56/S13/WeiGuo/battleship/model/Ship.java
+++ src/edu/ucsb/cs56/S13/WeiGuo/battleship/model/Ship.java
@@ -1,211 +1,306 @@
 package edu.ucsb.cs56.S13.WeiGuo.battleship.model;
 
-import java.util.*;
 
-/**
-   class that makes the ships
-   each ship has a String shipType that describes the type of ship it is, and a Map<Integer, Boolean> shipBody that describes the status of each section of the ship
-*/
-
-public class Ship {
-    
-    // Possible ship types
-    public static final String CARRIER     = "CARRIER";
-    public static final String BATTLESHIP  = "BATTLESHIP";
-    public static final String SUBMARINE   = "SUBMARINE";
-    public static final String DESTROYER   = "DESTROYER";
-    public static final String PATROL_BOAT = "PATROL_BOAT";
-    
-    private Map<Integer, Boolean> shipBody;   // map to keep track of ship's parts
-    private String shipType;                  // a character to denote the ship type
-    
-    /**
-       Constructs a ship based on a ship type.
-       @param shipType one of 5 possible ships: CARRIER, DESTROYER, SUBMARINE, BATTLESHIP, PATROL_BOAT
-       @param location an integer from 00-99 to represent the ships location
-       @param orientation a character (U,D,L,R) to represent the ships orientation
-    */
-    public Ship(String shipType, int location, String orientation){
-	shipBody      = new HashMap<Integer, Boolean>();
-	this.shipType = "" + shipType.charAt(0);
-
-	//determine how big to build each ship
-	if(shipType.equals(Ship.CARRIER)) {
-	    if(orientation.equals("U")){
-		for(int i = 0; i < 5; i++){
-		    shipBody.put(location - 10 * i, true);
-		}
-	    }
-	    
-	    else if(orientation.equals("D")){
-		for(int i = 0; i < 5; i++){
-		    shipBody.put(location + 10 * i, true);
-		}
-	    }
-	    
-	    else if(orientation.equals("L")){
-		for(int i = 0; i < 5; i++){
-		    shipBody.put(location - i, true);
-		}
-	    }
-
-	    else if(orientation.equals("R")){
-		for(int i = 0; i < 5; i++){
-		    shipBody.put(location + i, true);
-		}
-	    }
-		    
-
+import javax.swing.*;
+import java.awt.*;	
+	
+public class Ship
+{		
+	private String name;
+	private int dir=5,
+			   length,				   
+			   i,
+			   j,				   
+			   x1,
+			   y1,
+			   x2,
+			   y2;
+	private int hitsleft;
+	private boolean invalid;
+	
+	public Ship(String n, int d, int ln, int x, int y)
+	{
+		name=n;
+		length=ln;
+		dir=d;
+		x1=x;
+		y1=y;
+		invalid=false;
+		hitsleft=ln;			
 	}
-	else if(shipType.equals(Ship.BATTLESHIP)) {
-	    if(orientation.equals("U")){
-		for(int i = 0; i < 4; i++){
-		    shipBody.put(location - 10 * i, true);
-		}
-	    }
-	    
-	    else if(orientation.equals("D")){
-		for(int i = 0; i < 4; i++){
-		    shipBody.put(location + 10 * i, true);
-		}
-	    }
-	    
-	    else if(orientation.equals("L")){
-		for(int i = 0; i < 4; i++){
-		    shipBody.put(location - i, true);
-		}
-	    }
+	
+	public Ship(String n, int d, int ln, int x, int y, int ex, int ey)
+	{
+		name=n;
+		length=ln;
+		dir=d;
+		x1=x;
+		y1=y;
+		x2=ex;
+		y2=ey;
+		invalid=false;
+		hitsleft=ln;			
+	}
+	
+	public String getName()
+	{
+		return this.name;
+	}		
+	
+	public int getLength()
+	{
+		return this.length;
+	}
+	
+	public int getDirect()
+	{
+		return this.dir;
+	}
+	
+	public int getX()
+	{
+		return this.x1;
+	}
+	
+	public int getY()
+	{
+		return this.y1;
+	}
+	
+	//returns the end x-point for this ship 
+	public int getEndX()
+	{
+		return this.x2;
+	}
+	
+	//returns the end y-point for this ship 
+	public int getEndY()
+	{
+		return this.y2;
+	}
 
-	    else if(orientation.equals("R")){
-		for(int i = 0; i < 4; i++){
-		    shipBody.put(location + i, true);
-		}
-	    }
+	public void setInvalid(boolean c)
+	{				
+		this.invalid=c;
 	}
-	else if(shipType.equals(Ship.SUBMARINE)) {
-	    if(orientation.equals("U")){
-		for(int i = 0; i < 3; i++){
-		    shipBody.put(location - 10 * i, true);
-		}
-	    }
-	    
-	    else if(orientation.equals("D")){
-		for(int i = 0; i < 3; i++){
-		    shipBody.put(location + 10 * i, true);
-		}
-	    }
-	    
-	    else if(orientation.equals("L")){
-		for(int i = 0; i < 3; i++){
-		    shipBody.put(location - i, true);
-		}
-	    }
-
-	    else if(orientation.equals("R")){
-		for(int i = 0; i < 3; i++){
-		    shipBody.put(location + i, true);
-		}
-	    }
+			
+	public void setHitsLeft()
+	{				
+		this.hitsleft-=1;
 	}
-	else if(shipType.equals(Ship.DESTROYER)) {
-	    if(orientation.equals("U")){
-		for(int i = 0; i < 3; i++){
-		    shipBody.put(location - 10 * i, true);
+	
+	public int getHitsLeft()
+	{				
+		return this.hitsleft;
+	}
+	
+	public void clearship ()
+	{				
+		switch (this.dir)
+		{
+			case 0:	{													
+						if  (!this.invalid)
+							for (j=this.y1;j<this.y2;j++)
+							{
+								Battleship.getPlayers(Battleship.getYou()).setBboard(this.x1,j,null);
+								Battleship.getPlayers(Battleship.getYou()).setHitOrMiss(this.x1,j,false);
+								Battleship.getPlayers(Battleship.getYou()).setWhatShip(this.x1,j," ");	
+							}
+					}
+			break;
+			case 1:	{	
+						if (!this.invalid)	
+							for (i=this.x1;i<this.x2;i++)
+							{
+								Battleship.getPlayers(Battleship.getYou()).setBboard(i,this.y1,null);
+								Battleship.getPlayers(Battleship.getYou()).setHitOrMiss(i,this.y1,false);
+								Battleship.getPlayers(Battleship.getYou()).setWhatShip(i,this.y1," ");	
+							}								
+					}
+			break;				
 		}
-	    }
-	    
-	    else if(orientation.equals("D")){
-		for(int i = 0; i < 3; i++){
-		    shipBody.put(location + 10 * i, true);
+	}
+	
+	//Method to place the ships	
+	public void placeship()
+	{				
+		switch (this.dir)
+		{
+			case 0:	{												
+						if ((this.length+this.y1)>10)								
+						{
+							JOptionPane.showMessageDialog(null,"A "+
+							this.name+" placed in a "+Battleship.getDirection(this.dir)+
+							" direction will not fit at position "
+							+Battleship.getCletters(this.x1+1)+Battleship.getCnumbers(this.y1+1)+".",
+							"Invalid Placement",JOptionPane.ERROR_MESSAGE);
+							this.invalid=true;
+						}   								
+						else
+						{												
+							j=0;
+							while ((j!=this.length)&&(!Battleship.getPlayers(Battleship.getYou()).getHitOrMiss(this.x1,this.y1+j)))
+								j++;
+							if (j!=this.length)
+							{
+								JOptionPane.showMessageDialog(null,"Positio"
+								+"n "+Battleship.getCletters(this.x1+1)+
+								Battleship.getCnumbers(this.y1+j+1)+" is already occupied.",
+								"Invalid Placement",JOptionPane.ERROR_MESSAGE);
+								this.invalid=true;
+							}
+							else
+							{
+								this.x2=this.x1;
+								this.y2=this.y1+this.length;								
+								for (j=this.y1;j<this.y2;j++)
+								{
+									Battleship.getPlayers(Battleship.getYou()).setBboard(this.x1,j,Battleship.getColor());
+									Battleship.getPlayers(Battleship.getYou()).setHitOrMiss(this.x1,j,true);
+									Battleship.getPlayers(Battleship.getYou()).setWhatShip(this.x1,j,this.name);										
+								}
+								this.invalid=false;
+							}
+						}
+					}
+			break;
+			case 1:	{		
+						if ((this.x1+this.length)>10)								
+						{
+							JOptionPane.showMessageDialog(null,"A "+
+							this.name+" placed in a "+Battleship.getDirection(this.dir)+
+							" direction will not fit at position "
+							+Battleship.getCletters(this.x1+1)+Battleship.getCnumbers(this.y1+1)+".",
+							"Invalid Placement",JOptionPane.ERROR_MESSAGE);
+							this.invalid=true;
+						}
+						else
+						{							
+							j=0;
+							while ((j!=this.length)
+								&&(!Battleship.getPlayers(Battleship.getYou()).getHitOrMiss(this.x1+j,this.y1)))
+								j++;
+							if (j!=this.length)
+							{
+								JOptionPane.showMessageDialog(null,"Positio"
+								+"n "+Battleship.getCletters(this.x1+j+1)+
+								Battleship.getCnumbers(this.y1+1)+" is already occupied.",
+								"Invalid Placement",JOptionPane.ERROR_MESSAGE);
+								this.invalid=true;
+							}
+							else
+							{
+								this.y2=this.y1;
+								this.x2=this.x1+this.length;										
+								for (i=this.x1;i<this.x2;i++)
+								{
+									Battleship.getPlayers(Battleship.getYou()).setBboard(i,this.y1,Battleship.getColor());
+									Battleship.getPlayers(Battleship.getYou()).setHitOrMiss(i,this.y1,true);
+									Battleship.getPlayers(Battleship.getYou()).setWhatShip(i,this.y1,this.name);				
+								}
+								this.invalid=false;
+							}
+						}
+					}
+			break;							
+		}			
+		if ((Battleship.getW()>0)&&(Battleship.getA()>0)
+			&&(Battleship.getS()>0)&&(Battleship.getT()>0)
+		&&(Battleship.getE()>0)&&(!this.invalid))				
+		{	
+			if ((!Battleship.getPlayers(Battleship.getYou()).getBoats(0).invalid)&&(!Battleship.getPlayers(Battleship.getYou()).getBoats(1).invalid)&&(!Battleship.getPlayers(Battleship.getYou()).getBoats(2).invalid)
+				&&(!Battleship.getPlayers(Battleship.getYou()).getBoats(3).invalid)&&(!Battleship.getPlayers(Battleship.getYou()).getBoats(4).invalid))
+				Battleship.setDeploy(true);
+			else
+				Battleship.setDeploy(false);
 		}
-	    }
-	    
-	    else if(orientation.equals("L")){
-		for(int i = 0; i < 3; i++){
-		    shipBody.put(location - i, true);
-		}
-	    }
-
-	    else if(orientation.equals("R")){
-		for(int i = 0; i < 3; i++){
-		    shipBody.put(location + i, true);
-		}
-	    }
+		else
+			Battleship.setDeploy(false);
 	}
-	else if(shipType.equals(Ship.PATROL_BOAT)) {
-	    	    if(orientation.equals("U")){
-		for(int i = 0; i < 2; i++){
-		    shipBody.put(location - 10 * i, true);
-		}
-	    }
-	    
-	    else if(orientation.equals("D")){
-		for(int i = 0; i < 2; i++){
-		    shipBody.put(location + 10 * i, true);
-		}
-	    }
-	    
-	    else if(orientation.equals("L")){
-		for(int i = 0; i < 2; i++){
-		    shipBody.put(location - i, true);
-		}
-	    }
-
-	    else if(orientation.equals("R")){
-		for(int i = 0; i < 2; i++){
-		    shipBody.put(location + i, true);
-		}
-	    }
+	
+	public Ship compinput(int u, int n)
+	{			
+		Ship boat;
+		
+		int i=0,
+			j=0,
+			x,
+			y,
+			shipl=0,	
+			dir;
+		
+		switch (u)
+		{	
+			case 0:		shipl=5;
+			break;
+			case 1:		shipl=4;
+			break;
+			case 2:			
+			case 3:		shipl=3;
+			break;
+			case 4:		shipl=2;
+			break;							
+		}		
+		
+		do
+		{
+			x=(int)(Math.random()*10);
+			y=(int)(Math.random()*10);				
+			dir=(int)(Math.random()*2);//generates random direction within range			
+			boat=new Ship(Battleship.getShips(u),dir,shipl,x,y);				
+			switch (dir)
+			{
+				case 0:	{												
+							if (((boat.getLength()+y)>10)||(x==0)||(y==0))								
+								boat.setInvalid(true);																				
+							else
+							{												
+								j=0;									
+								while ((j!=boat.getLength())&&(!Battleship.getPlayers(n).getHitOrMiss(x,y+j)))
+									j++;								
+								if (j!=boat.getLength())
+									boat.setInvalid(true);																		
+								else
+								{
+									boat.x2=x;
+									boat.y2=y+boat.getLength();								
+									for (j=y;j<boat.y2;j++)
+									{										
+										Battleship.getPlayers(n).setHitOrMiss(x,j,true);
+										Battleship.getPlayers(n).setWhatShip(x,j,Battleship.getShips(u));				
+									}
+									boat.setInvalid(false);																
+								}
+							}
+						}
+				break;
+				case 1:	{		
+							if (((x+boat.getLength())>10)||(x==0)||(y==0))						
+								boat.setInvalid(true);							
+							else
+							{							
+								j=0;									
+								while ((j!=boat.getLength())&&(!Battleship.getPlayers(n).getHitOrMiss(x+j,y)))
+									j++;
+								if (j!=boat.getLength())
+									boat.setInvalid(true);							
+								else
+								{
+									boat.y2=y;
+									boat.x2=x+boat.getLength();										
+									for (i=x;i<boat.x2;i++)
+									{
+										Battleship.getPlayers(n).setHitOrMiss(i,y,true);
+										Battleship.getPlayers(n).setWhatShip(i,y,Battleship.getShips(u));			
+									}
+									boat.setInvalid(false);									
+								}
+							}
+						}
+				break;						
+			}			
+		}			
+		while (boat.invalid);		
+		return boat;
 	}
-	else {
-	    System.err.println("Invalid Ship Type");
-	}
-    }
-
-    public int getLength(){
-	return shipBody.size();
-    }
-    
-    public String getShipType() {
-	return shipType;
-    }
-
-    /**
-       Updates the ship to reflect a hit at the specified location
-       @param location The int representing the area of the ship hit
-    */
-    public void hit(int location) {
-	shipBody.put(location, false);
-    }
-
-    /**
-       Determines if the ship has been sunk
-    */
-    public boolean isSunk(){
-	if(shipBody.containsValue(true)){
-	    return false;
-	}
-	else return true;
-    }
-
-    public String getFullShipName(){
-	String type = "";
-      
-	if(shipType.equals("C")){
-	    type = "CARRIER";
-	}
-	if(shipType.equals("B")){
-	    type = "BATTLESHIP";
-	}
-	if(shipType.equals("D")){
-	    type = "DESTROYER";
-	}
-	if(shipType.equals("S")){
-	    type = "SUBMARINE";
-	}
-	if(shipType.equals("P")){
-	    type = "PATROL_BOAT";
-	}
-	return type;		
-    }
 }
