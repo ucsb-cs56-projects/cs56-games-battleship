65ec97ddf1868841104868f06176ad85494a252e
Keenan Reimer <kreimer@umail.ucsb.edu>
2013-05-28 22:41:54 -0700

    Added random boat generation methods and began refactoring guiController

--- src/edu/ucsb/cs56/projects/games/battleship/model/Player.java
+++ src/edu/ucsb/cs56/projects/games/battleship/model/Player.java
@@ -2,6 +2,7 @@ package edu.ucsb.cs56.projects.games.battleship;
 
 import java.util.*;
 import java.io.*;
+import java.lang.Math.*;
 
 /**
    The player class, holds a gameboard and takes in player input
@@ -9,104 +10,86 @@ import java.io.*;
 
 public class Player {
     
-    private GameBoard gb;
-    private boolean isWinner;
+    private ArrayList<Integer> boats;
+    private ArrayList<Integer> shots;
     
     /**
        default constructor
     */
     public Player(){
-	gb = new GameBoard();
-	isWinner = false;
+		boats = new ArrayList<Integer>();
+		shots = new ArrayList<Integer>();
     }
-    
-	public ArrayList getShipLocations(){
-		return this.gb.getShipLocations();
+	
+	public void randomGenerateBoats(){
+		int[] shipSizes = {2,3,3,4,5};
+		for( int shipSize: shipSizes){
+			int spawn = (int) (99.99*Math.random());
+			int orientation = (int) (3.99*Math.random());
+			while( !isValidSpawn( spawn, orientation, shipSize))
+				spawn = (int) (99.99*Math.random());
+			addShip( spawn, orientation, shipSize);
+		}
+		
 	}
 	
-    /**
-       Requests a move from the player
-       @return an int from 0-99 corresponding to the player's move or -1 if error
-    */
-    public int requestMove() {
-	// prompt the user for a move
-	System.out.println("Where to fire missiles?");
+	public boolean isValidLocal(int i){
+		if( i >= 0 && i <= 99 ) return true;
+		else return false;
+	}
 	
-	//  open up standard input
-	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-	String playersMove = null;
-
-	//  read the move from the command-line; need to use try/catch with the
-	//  readLine() method
-	try {
-	    playersMove = br.readLine();
-	} catch (IOException ioe) {
-	    System.err.println("IO error trying to read your move!" + ioe.getMessage());
-	    return -1;
-	}
-	// check to see if the move is a valid location (A0-J9)
-	if(playersMove.length() != 2) {
-	    System.err.println("Invalid Move");
-	    return -1;
-	}
-	else {
-	    playersMove = playersMove.toUpperCase();
-	    try {
-		int char1 = (int)playersMove.charAt(0);
-		int char2 = Integer.parseInt(""+playersMove.charAt(1));
-	    
-		// A = 65, J = 74
-		if(char1 < 65 || char1 > 74 || char2 < 0 || char2 > 9) {
-		    System.err.println("Invalid Move");
-		    return -1;
+	public boolean isValidSpawn(int spawn, int orientation, int shipSize){
+		if( orientation == 0){ //up
+			for( int i=0; i<shipSize; i++){
+				if( !isValidLocal(spawn - 10*i) || boats.contains(spawn - 10*i)) return false;
+				else return true;
+			}
 		}
-		// converts playersMove to an int from 0-99
-		int location = (char1-65)*10 + char2;
-
-		// checks to see if location has already been guessed
-		if(gb.alreadyGuessed(location)) {
-		    System.err.println("Already guessed " + playersMove);
-		    return -1;
+		if( orientation == 1){ //right
+			for( int i=0; i<shipSize; i++){
+				if( !isValidLocal(spawn + i) || boats.contains(spawn + i)) return false;
+				else return true;
+			}
 		}
-		return location;
-	    }
-	    catch (NumberFormatException nfe) {
-		System.err.println("Invalid Move");
-		return -1;
-	    }
+		if( orientation == 2){ //down
+			for( int i=0; i<shipSize; i++){
+				if( !isValidLocal(spawn - 10*i) || boats.contains(spawn - 10*i)) return false;
+				else return true;
+			}
+		}
+		if( orientation == 3){ //left
+			for( int i=0; i<shipSize; i++){
+				if( !isValidLocal(spawn - i) || boats.contains(spawn - i)) return false;
+				else return true;
+			}
+		}
+		return false;
 	}
-    }
-
-    /**
-       sends a missle down to the gameboard
-       @param location the location being attacked by the opponent
-       @return status of the missle "HIT", "MISS" or "SUNK shiptype"
-    */
-    public String incomingMissile(int location) {
-        return gb.incomingMissile(location);
-    }
-
-    /**
-       updates the guessgrid to reflect the status at location
-       @param location the location to be updated
-       @param status "HIT", "MISS", "SUNK shiptype", or "WIN"
-    */
-    public void updateGuessGrid(int location, String status) {
-	if(status.equals("WIN"))
-	    isWinner = true;
-	else
-	    gb.updateGuessGrid(location, status);
-    }	
-    
-    /**
-       @return if this player is a winner
-    */
-    public boolean hasWon() {
-	return isWinner;
-    }
-
-    public void printGameBoard() {
-	System.out.println(gb.toString());
-    }
+	
+	public void addShip(int spawn, int orientation, int shipSize){
+		if( orientation == 0){ //up
+			for( int i=0; i<shipSize; i++){
+				boats.add(spawn - 10*i);
+			}
+		}
+		if( orientation == 1){ //right
+			for( int i=0; i<shipSize; i++){
+				boats.add(spawn + i);
+			}
+		}
+		if( orientation == 2){ //down
+			for( int i=0; i<shipSize; i++){
+				boats.add(spawn - 10*i);
+			}
+		}
+		if( orientation == 3){ //left
+			for( int i=0; i<shipSize; i++){
+				boats.add(spawn - i);
+			}
+		}
+	}
+	
+	public ArrayList<Integer> getBoatList(){
+		return this.boats;
+	}
 }
