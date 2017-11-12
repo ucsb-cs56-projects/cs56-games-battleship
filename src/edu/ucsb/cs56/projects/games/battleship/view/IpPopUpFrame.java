470e21404a1df86b191bd04449bc4bd9f79e3af2
Kindy Tan <ktan@umail.ucsb.edu>
2017-11-11 23:10:56 -0800

    slightly improved the gui

--- src/edu/ucsb/cs56/projects/games/battleship/view/IpPopUpFrame.java
+++ src/edu/ucsb/cs56/projects/games/battleship/view/IpPopUpFrame.java
@@ -8,24 +8,31 @@ import java.io.*;
 import javax.sound.sampled.*;
 
 public class IpPopUpFrame extends JFrame implements ActionListener{
+
+	ImageIcon icon = new ImageIcon(getClass().getResource("images/Wifi-icon.png"));
+
 	private JLabel ipRequest = new JLabel("Please input the IP address you wish to join.", JLabel.CENTER);
 	private JTextField ipField = new JTextField();
 	private JLabel ipMessage = new JLabel("Hit enter to submit.", JLabel.CENTER);
+	private JLabel ipIcon = new JLabel(icon);
 	private boolean ipEntered = false;
 
 	public IpPopUpFrame(){
 		        //Setup IP popup
-        GridLayout threeWidgetVerticleGrid = new GridLayout(3,1);
+        GridLayout threeWidgetVerticleGrid = new GridLayout(4,1);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(threeWidgetVerticleGrid);
-        this.setSize(600,100);
+        this.setSize(600,200);
         this.ipField.setHorizontalAlignment(JTextField.CENTER);
         this.ipField.addActionListener(this);
 
         //Add IP widgets
+		
         this.getContentPane().add(BorderLayout.NORTH,ipRequest);
         this.getContentPane().add(BorderLayout.CENTER,ipField);
         this.getContentPane().add(BorderLayout.SOUTH,ipMessage);
+		this.getContentPane().add(BorderLayout.SOUTH,ipIcon);
+		
 	}
 
 	public void actionPerformed(ActionEvent e){
